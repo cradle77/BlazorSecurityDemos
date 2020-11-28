e69f9db0043dc5aaa7e8e651acd2ff28b80fdb59
cradle77 <marcodesanctis2@gmail.com>
2020-11-28 10:13:11 +0000

    port to .net 5.0

--- WebAssembly/Program.cs
+++ WebAssembly/Program.cs
@@ -13,7 +13,7 @@ namespace WebAssembly
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services
                 .AddHttpClient("weatherapi", client =>
